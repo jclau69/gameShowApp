a2ddb54aa59f1ef82d5df9e789403946f5d9626a
jclau69 <jclau69@outlook.com>
2020-09-28 11:33:34 +0200

    selecting starting button

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var missed = 0;
 const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const buttonStart = document.getElementById('overlay');
+
+
+buttonStart.addEventListener('click', (e) => {
+
+console.log('hi');
+
+
+
+});
