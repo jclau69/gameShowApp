8920454184b97d6776478c6652c90feb1294ba5f
jclau69 <jclau69@outlook.com>
2020-09-29 09:43:49 +0200

    function array

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 var missed = 0;
+var phrases = [
+  'hold on to your hat',
+  'birds of a feather flock together',
+  'a turn up for the books','the world is your oyster',
+  'by hook or by crook']
+
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const buttonStartDiv = document.getElementById('overlay');
 
+function getRandomPhraseAsArray(){
+const randomNumb = Math.floor(Math.random() * 5);
+return randomNumb;
 
+}
 
+function findAphraseInArray(array){
+
+for(let i = 0; i < array.length; i++){
+
+let phraseArray = array[i];
+return phraseArray;
+}
+
+}
 
 buttonStartDiv.addEventListener('click', (e) => {
 
 let title = buttonStartDiv.firstElementChild;
 title.style.display = 'none';
 buttonStartDiv.style.display = 'none';
-
 
 });
