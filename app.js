46ec90de57b79be1a1f9b1f669d7617642e3e525
jclau69 <jclau69@outlook.com>
2020-09-29 18:09:30 +0200

    function addPhraseToDisplay editing

--- app.js
+++ app.js
@@ -2,27 +2,53 @@ var missed = 0;
 var phrases = [
   'hold on to your hat',
   'birds of a feather flock together',
-  'a turn up for the books','the world is your oyster',
-  'by hook or by crook']
+  'a turn up for the books',
+  'the world is your oyster',
+  'by hook or by crook',
+  'got a lot of nerves']
 
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+
 const buttonStartDiv = document.getElementById('overlay');
 
-function getRandomPhraseAsArray(){
-const randomNumb = Math.floor(Math.random() * 5) ;
+
+function getRandom(){
+  // get a random number.
+
+const randomNumb = Math.floor(Math.random() * (phrases.length)) ;
 return randomNumb;
 
 }
 
-function findAphraseInArray(array){
+function getRandomPhraseAsArray(){
+// get a phrase from the Array and split it into characters.
 
-for(let i = 0; i < array.length; i++){
+for(let i = 0; i < phrases.length; i++){
 
- let result = (getRandomPhraseAsArray(array.indexOf([i])));
+ let result = getRandom(phrases.indexOf([i]));
+ let phraseFound = phrases[result];
+ let phraseSplit = phraseFound.split("");
 
-return result;
+
+ return phraseSplit;
+
 }
+  }
+
+  function addPhraseToDisplay(){
+    // do stuff any arr that is passed in, and add to `#phrase ul.
+    const phraseDiv = document.getElementById('phrase');
+    let ul = phraseDiv.firstElementChild;
+    let li = document.createElement("li");
+
+    for(let i = 0; i < getRandomPhraseAsArray().length; i++){
+
+      return getRandomPhraseAsArray();
+
+    }
+
+    li.appendChild(characterSplit);
+    ul.appendChild(li);
 
 }
 
