153f9df817f34bf6f3a26cce3b23a063de4c5e08
jclau69 <jclau69@outlook.com>
2020-10-08 16:16:18 +0200

    adding button reload functionality

--- app.js
+++ app.js
@@ -166,7 +166,10 @@ if (score === 5) {
 
 buttonStartDiv.addEventListener('click', (e) => {
 
+if(e.target.textContent === 'Play Again'){
 
+  window.location.reload();
+}
 
 let title = buttonStartDiv.firstElementChild;
 title.style.display = 'none';
